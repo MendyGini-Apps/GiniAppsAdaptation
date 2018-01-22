a31b12b9d78608595773b03e9b6fbd0011f80bf6
Menahem Barouk <menahemb@gini-apps.com>
2018-01-22 13:13:23 +0200

    initial commit

---
+++ GiniAppsAdaptation/PeopleHeaderSection.swift
@@ -0,0 +1,46 @@
+//
+//  PeopleHeaderSection.swift
+//  GiniAppsAdaptation
+//
+//  Created by Menahem Barouk on 22/01/2018.
+//  Copyright © 2018 Gini-Apps. All rights reserved.
+//
+
+import UIKit
+
+
+
+class PeopleHeaderSection: UITableViewCell {
+    
+    static let nibName = "PeopleHeaderSection"
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var portraitImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        roundLayerImage()
+    }
+    
+    private func roundLayerImage() {
+        portraitImageView.layer.cornerRadius = min(portraitImageView.bounds.width, portraitImageView.bounds.height)/2
+        portraitImageView.layer.masksToBounds = true
+    }
+    
+    func configure(withName name: String?, gender: String?, height: String?) {
+        nameLabel.text = name
+        genderLabel.text = gender
+        heightLabel.text = height
+
+        if let name = name,
+            let image = UIImage(named: name) {
+            portraitImageView.image = image
+        } else {
+            portraitImageView.image = nil
+        }
+    }
+    
+}
