b4a727624e83ccb544d0f25e00a4e64e518000e7
Menahem Barouk <menahemb@gini-apps.com>
2018-01-22 10:26:13 +0200

    initial commit

---
+++ GiniAppsAdaptation/Model/People.swift
@@ -0,0 +1,20 @@
+//
+//  People.swift
+//  GiniAppsAdaptation
+//
+//  Created by Menahem Barouk on 21/01/2018.
+//  Copyright © 2018 Gini-Apps. All rights reserved.
+//
+
+import UIKit
+
+struct People: Decodable {
+    let name: String?
+    let height: String?
+    let gender: String?
+}
+
+struct Result: Decodable {
+    let results: [People]?
+    let count: Int?
+}
